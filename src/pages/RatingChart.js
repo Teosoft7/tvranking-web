68b9ab0ddd8cb091c31081d15ffda9315b6871ad
Teosoft7 <thjeon.wa@gmail.com>
2022-10-25 10:11:44 -0700

    add list/chart and mui

---
+++ src/pages/RatingChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RatingChart = () => {
+  return (
+    <div>
+      <h5>Rating Chart</h5>
+    </div>
+  );
+};
+
+export default RatingChart;
---
+++ src/pages/RatingList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RatingList = () => {
+  return (
+    <div>
+      <h5>Rating List</h5>
+    </div>
+  );
+};
+
+export default RatingList;
