d8f44338b2643c5d60bfecd1ccc0f5f5334a156e
Teosoft7 <thjeon.wa@gmail.com>
2022-11-08 14:15:42 -0800

    add router and info page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
 import Layout from "./layout/layout";
 import HomePage from "./pages/Home";
+import InfoPage from "./pages/Info";
 
 function App() {
   return (
-    <Layout>
-      <HomePage />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<HomePage />}></Route>
+          <Route exact path="/about" element={<InfoPage />}></Route>
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
 export default App;
+
+// <Router>
+// <div className="App">
+//  <ul className="App-header">
+//    <li>
+//      <Link to="/">Home</Link>
+//    </li>
+//    <li>
+//      <Link to="/about">About Us</Link>
+//    </li>
+//    <li>
+//      <Link to="/contact">Contact Us</Link>
+//    </li>
+//  </ul>
+// <Routes>
+//       <Route exact path='/' element={< Home />}></Route>
+//       <Route exact path='/about' element={< About />}></Route>
+//       <Route exact path='/contact' element={< Contact />}></Route>
+// </Routes>
+// </div>
+// </Router>
---
+++ src/pages/Info.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InfoPage = () => {
+  return (
+    <div>
+      <h2> TV RANKING INFO </h2>
+      <h5> This is a information page for TVRanking</h5>
+    </div>
+  );
+};
+
+export default InfoPage;
